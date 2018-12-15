9896be46e8a1384ca95667691cc2fe6228010f3b
morrinxx <marcel.poelzl@hotmail.com>
2018-12-15 12:32:53 +0100

    first commit

---
+++ Poelzl_M/Processes/parallel.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+char* ls = "ls";
+char* df = "df";
+char* ps = "ps";
+char* pwd = "pwd";
+
+
+int main(int argc, char const *argv[]) {
+
+  return 0;
+}
