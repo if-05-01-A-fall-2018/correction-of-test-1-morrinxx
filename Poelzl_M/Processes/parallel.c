45e841c9ad841a16219f878397c672edb46b61c5
morrinxx <marcel.poelzl@hotmail.com>
2018-12-15 12:45:23 +0100

    Added basic stucture

--- Poelzl_M/Processes/parallel.c
+++ Poelzl_M/Processes/parallel.c
@@ -2,13 +2,24 @@
 #include <string.h>
 #include <unistd.h>
 
-char* ls = "ls";
-char* df = "df";
-char* ps = "ps";
-char* pwd = "pwd";
+char* arguments[] = {"ls", "df", "ps", "pwd"};
 
 
 int main(int argc, char const *argv[]) {
+  int count = 0;
+  while (arguments[count] != 0) {
+    int pid = fork();
+    if(pid == 0){
+      execvp(arguments[count], arguments);
+    }
+    else{
+      int status;
+      waitpid(-1, &status, 0);
+    }
+
+    count++;
+  }
+  printf("Done\n");
 
   return 0;
 }
